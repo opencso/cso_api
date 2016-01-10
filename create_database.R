1dc381d7e4cb19e11a14a53d175c702ef1e6e6de
David Morrisroe <pizzadave108@gmail.com>
2016-01-10 18:17:52 +0000

    now creating correct sql

--- create_database.R
+++ create_database.R
@@ -16,14 +16,15 @@ require(JSON)
 require(jsonlite)
 
 
-
-generate.sql <- function(){
+all_sql <- ""
+for(k in 3:6){
     
     file_path <- "/home/dave/cso/api/files/"
     table_list <- read.csv("table_names.csv")
-    current_filename <- table_list[2,]
+    current_filename <- table_list[k,]
     current_dataset <- paste0(file_path, current_filename)
     my <- fromJSONstat(current_dataset)
+    my_codes <-fromJSONstat(current_dataset, naming="id")
     
     ## grab a copy of the json file to pull out metadata
     just_json <- fromJSON(current_dataset)
@@ -99,33 +100,38 @@ generate.sql <- function(){
 
     my$value <- as.numeric(my$value)
    
-    write.table(my, file=paste0("/home/dave/cso/api/output/",current_filename), col.names=FALSE, sep=",")
+    write.table(my, file=paste0("/home/dave/cso/api/output/",current_filename, ".csv"),na="NULL", col.names=FALSE, sep=",")
     ## create
-    all_sql <- ""
+    
     sql_string <-  paste0("create table ", current_filename, "( index int,\n")
 
-    for(i in 1:length(set_ids)){
-        if(is.factor(my[,i])){
+    for(i in 0:length(set_ids)+1) {
+        names(my)[i] <- gsub("\\.", "_", names(my)[i])
+        cat(i, " ",names(my)[i], "\n")
+        if(is.factor(my[,i])) {
+            ##cat(names(my)[i], " is a factor\n")
             names(my)[i]
             my_levels <- levels(my[,i])
             max_length <- 0
             for(j in 1:length(my_levels)){
                 if(nchar(as.character(my_levels[j])) > max_length){
                     max_length <- nchar(as.character(my_levels[j]))
-                }
-                sql_string <- paste0(sql_string, names(my)[i], " varchar(", max_length, "),\n")
-                
+                }                
             }
-        } else if(i == time_index){
+            sql_string <- paste0(sql_string, names(my)[i], " varchar(", max_length, "),\n")
+        } else if(i == time_index) {
+            ##cat(names(my)[i], " is a date\n")
             sql_string <- paste0(sql_string, names(my)[i], " date,\n")
-        } else if(names(my)[i] == "value"){
-            sql_string <- paste0(sql_string, "value real\n);\n")
+        } else if(names(my)[i] == "value") {
+            ##cat(names(my)[i], " is the value\n")
+            sql_string <- paste0(sql_string,current_filename, "_value real\n);\n")
                 
         }
 
     }
-    sql_string <- paste0(sql_string, "\\copy ", current_filename, "FROM '/home/dave/cso/api/output/", current_filename,".csv' DELIMITER ',' CSV;\n\n")
+    sql_string <- paste0(sql_string, "\\copy ", current_filename, " FROM '/home/dave/cso/api/output/", current_filename,".csv' DELIMITER ',' CSV WITH NULL AS NA ;\n\n")
     all_sql <- paste0(all_sql, sql_string)
-    cat(all_sql, file="all_sql.csv")
     
 }
+
+cat(all_sql, file="all_sql.sql")    
